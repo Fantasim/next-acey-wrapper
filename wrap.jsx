947313472b838a52a1d8b3fb01c9eb791846814b
Louis <fantasim.dev@gmail.com>
2020-05-16 15:44:26 +0200

    1.1.0 working with acey 1.1.1

--- wrap.jsx
+++ wrap.jsx
@@ -1,67 +1,67 @@
 import React from 'react'
 import _ from 'lodash'
 import * as Cookies from 'es-cookie'
-import { config, store } from 'acey'
 
-const STORE_KEY = '_aceyStore'
-config.setEnvAsNextJS()
-
-export const withAcey = (App) => {
-
+export const withAcey = (App, Acey) => {
+    const { manager, config } = Acey
+    
+    config.setEnvAsNextJS()
+    config.done()
+    const STORE_KEY = '_aceyStore'
+  
     return class Wrap extends React.Component {
-        
-        constructor(props){
-            super(props)
-            
-            !this.isServer() && store.addPendingHydration(props.pageProps[STORE_KEY]) 
-        }
-  
-        static getInitialProps = async ({ Component, router, ctx }) => {
-            let pageProps = {}
-  
-            if (!ctx) throw new Error('No page context');
-            const prevInitialPropsFunction = Component.getInitialProps
-  
-            if (ctx.req && ctx.req.headers && ctx.req.headers.cookie) {
-                const cookies = {}
-                const gotCookies = Cookies.parse(ctx.req.headers.cookie)
-                for (let key in gotCookies){
-                    if (store.exist(key))
-                        cookies[key] = JSON.parse(gotCookies[key])
-                }
-                store.hydrateCookies(cookies)
-            }
-            
-            if (Component.getInitialProps)
-                pageProps = await Component.getInitialProps(ctx)
-            
-            Component.getInitialProps = (ctx) => pageProps
-            Component.getInitialProps = prevInitialPropsFunction
-  
-            const ret = { 
-                pageProps: { ...pageProps }
-            }
-            ret.pageProps[STORE_KEY] = store.store()
-            return ret
-        }
-  
-        isServer = () => typeof window === 'undefined'
-        
-        getClearedProps = () => {
-            let newProps = {}
-            for (let key in this.props){
-                if (key === 'pageProps'){
-                    const copy = _.cloneDeep(this.props[key])
-                    delete copy[STORE_KEY]
-                    copy['isServer'] = this.isServer()
-                    newProps[key] = copy
-                } else {
-                    newProps[key] = this.props[key]
-                }
-            }
-            return newProps
-        }
-  
-        render = () => <App {...this.getClearedProps()} />
-    }
-}
+          
+          constructor(props){
+              super(props)
+              !this.isServer() && manager.addPendingHydration(props.pageProps[STORE_KEY]) 
+          }
+    
+          static getInitialProps = async ({ Component, router, ctx }) => {
+              let pageProps = {}
+    
+              if (!ctx) throw new Error('No page context');
+              const prevInitialPropsFunction = Component.getInitialProps
+      
+              if (ctx.req && ctx.req.headers && ctx.req.headers.cookie) {
+                  const cookies = {}
+                  const gotCookies = Cookies.parse(ctx.req.headers.cookie)
+                  for (let key in gotCookies){
+                      if (manager.exist(key))
+                          cookies[key] = JSON.parse(gotCookies[key])
+                  }
+                  manager.hydrateCookies(cookies)
+              }
+              
+              if (Component.getInitialProps)
+                  pageProps = await Component.getInitialProps(ctx)
+              
+              Component.getInitialProps = (ctx) => pageProps
+              Component.getInitialProps = prevInitialPropsFunction
+    
+              const ret = { 
+                  pageProps: { ...pageProps }
+              }
+              ret.pageProps[STORE_KEY] = manager.store()
+              return ret
+          }
+    
+          isServer = () => typeof window === 'undefined'
+          
+          getClearedProps = () => {
+              let newProps = {}
+              for (let key in this.props){
+                  if (key === 'pageProps'){
+                      const copy = _.cloneDeep(this.props[key])
+                      delete copy[STORE_KEY]
+                      copy['isServer'] = this.isServer()
+                      newProps[key] = copy
+                  } else {
+                      newProps[key] = this.props[key]
+                  }
+              }
+              return newProps
+          }
+    
+          render = () => <App {...this.getClearedProps()} />
+      }
+  }
